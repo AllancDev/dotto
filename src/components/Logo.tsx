c9de7973b6d24053883c1e157f8000ed3f4f2995
Allan Camargo <allan.cdev@gmail.com>
2022-12-19 02:48:16 -0300

    init repo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export function Logo() {
+  return (
+    <p style={{color: '#000', textDecoration: 'none'}}>Modo Viagem</p>
+  );
+}
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,7 @@
+import { ContactForm } from "src/components/ContactForm"
+
+export default function Contact() {
+    return (
+        <ContactForm />
+    )
+}
